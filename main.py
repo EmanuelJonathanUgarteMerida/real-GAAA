af3aa1028e386f96e08343304012a55e698c4a1c
Emanuel J. Ugarte Mérida <emanuelugartemerida@gmail.com>
2021-10-26 23:25:47 +0200

    REFACT

--- main.py
+++ main.py
@@ -4,24 +4,19 @@ import json
 from util import SpriteSheet
 
 pg.init
-clock=pg.time.Clock()
+clock = pg.time.Clock()
 pantalla = pg.display.set_mode((500, 500))
+sprite_sheet = SpriteSheet('pong.png', 'pong.json')
 
-sheetMostro = SpriteSheet('peleador.png')
+imagenes = []
+for data in sprite_sheet.data['paddle']['evo1']:
+    imagenes.append(sprite_sheet.get_image(
+        data['x'], data['y'], data['width'], data['height']))
 
-json_mostro_content=open('peleador.json','r')
-data=json.load(json_mostro_content)
-json_lines=json_mostro_content.readlines()
-json_mostro_content.close()
-imagenes=[]
-colorkey=data['colorkey']
-for i in data['rest']:
-	imagenes.append(sheetMostro.get_image(i['x'],i['y'],i['width'],i['height'],colorkey))
-imagen=imagenes[0]
-imagen_rect=imagen.get_rect()
-x=0
-contador=0
-pos=1
+image = imagenes[0]
+x = 0
+contador = 0
+pos = 1
 game_over = False
 while not game_over:
     clock.tick(60)
@@ -31,11 +26,11 @@ while not game_over:
                 game_over = True
             elif event.key == pg.K_RIGHT:
                 if pos < len(imagenes):
-                    imagen=imagenes[pos]
-                    pos+=1
+                    image = imagenes[pos]
+                    pos += 1
                 else:
-                    pos=0
-				
-    pantalla.fill((255,255,255))
-    pantalla.blit(imagen, imagen.get_rect())
+                    pos = 0
+
+    pantalla.fill((255, 255, 255))
+    pantalla.blit(image, image.get_rect())
     pg.display.flip()
--- util.py
+++ util.py
@@ -1,14 +1,27 @@
 import pygame as pg
+import json
 import os
 
 
 class SpriteSheet():
-    def __init__(self, name):
+    def __init__(self, name_img, name_json):
         self.sprite_sheet = pg.image.load(
-            os.path.join('png', name)).convert_alpha()
+            os.path.join('png', name_img)).convert_alpha()
+        self.data = self.leer_json(name_json)
+        self.colorkey = self.data['colorkey']
 
-    def get_image(self, x, y, width, height,colorkey):
+    def leer_json(self, file_name):
+        json_data = open(file_name, 'r')
+        d = json.load(json_data)
+        json_data.close()
+        return d
+
+    def get_image(self, x, y, width, height):
         image = pg.Surface((width, height)).convert_alpha()
         image.blit(self.sprite_sheet, (0, 0), (x, y, width, height))
-        image.set_colorkey((colorkey))
+        image = pg.transform.scale(image, (width*3, height*3))
+        image.set_colorkey((self.colorkey))
         return image
+
+    def get_image_compose(self):
+        return
